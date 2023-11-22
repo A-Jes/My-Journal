3a2ef6e05aab48d797411d8453c556be22fa96e3
Ajayi Sekemi <141793642+A-Jes@users.noreply.github.com>
2023-11-21 20:53:17 -0500

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,6 +1,13 @@
 import React from "react"
-
+ function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+          prevMeme,
+            [name]: value,
+        }));
+    }
 export default function Navbar (){
+    https://github.com/A-Jes/My-Journal.git
     return(
         <nav className="navbar" >
             <img src="../images/logo.png" alt="" className="navbar-img" />
